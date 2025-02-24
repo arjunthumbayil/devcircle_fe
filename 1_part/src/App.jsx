54e89c621406b120c13adea8b97ff4ac591da2de
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-24 22:24:42 +0530

    changes inside package.json

--- 1_part/src/App.jsx
+++ 1_part/src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
     </>
   );
 }
 
 export default App;
+
+//      <h1 className="text-3xl font-bold underline">Hello World</h1>
